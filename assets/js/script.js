79779ce5f407f84c2c988e0fb12b25b6c5601aab
Tom Chestnut <tchestnut85@gmail.com>
2020-09-11 22:37:02 -0400

    added a function searchEvent and event listener to enter the city searched for and call the weatherRequest function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,45 @@
+var searchButton = document.querySelector("#search-button");
+var searchBar = document.querySelector("#search-bar");
+var searchValue = searchBar.value;
+console.log(searchValue);
+
 
 // Make function to get weather api
 var weatherRequest = function(city) {
-    var weatherApi =  "https://api.openweathermap.org/data/2.5/weather?q=" + "philadelphia" + "&appid=c83c5006fffeb4aa44a34ffd6a27f135";
+    var weatherApi =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=c83c5006fffeb4aa44a34ffd6a27f135";
     // fetch the response
     fetch(weatherApi)
     .then(function(response) {
         return response.json();
     })
-    // grab the id of the element the weather data will be displayed
     .then(function(response) {
+        // log the fetch response
         console.log(response);
-    
-        var responseContainer = document.querySelector("current-result");
+        // grab the id of the element the weather data will be displayed
+        var responseContainer = document.querySelector("#current-result");
         // create element for the weather response
-        var weatherImg = document.createElement("img");
+        var cityName = document.createElement("span");
         // set the element's attribute to the json data path
-        // weatherImg.setAttribute("src", response.???);
-
+        cityName.setAttribute("src", response.name);
+        console.log(cityName);
         // append the element to html
-        responseContainer.appendChild(weatherImg);
-
-    })
-    // handle any errors
-    .catch(function(error) {
-        console.log(error)
-        alert("Something went wrong!")
-    });
+        responseContainer.appendChild(cityName);
+        
+        // handle any errors
+        // .catch(function(error) {
+        //     console.log(error)
+        //     alert("Something went wrong!")
+        // });
+    });  
 };
 
-weatherRequest();
+var searchEvent = function() {
+    // clicking search button submits value and calls weatherRequest function
+    console.log("test");
+    // event.target.querySelector("#search-button");
+    weatherRequest();
+};
+
+searchButton.addEventListener("click", searchEvent);
+
+// weatherRequest();
