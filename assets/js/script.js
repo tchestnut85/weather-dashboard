fa623fc0e239c05617ea81962cdc26a251f80255
Tom Chestnut <tchestnut85@gmail.com>
2020-09-11 23:44:48 -0400

    added a function to clear previously searched weather and updated search form css with bootstrap styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,61 @@
-var searchButton = document.querySelector("#search-button");
+var searchHandler = document.querySelector("#search-form");
 var searchBar = document.querySelector("#search-bar");
-var searchValue = searchBar.value;
-console.log(searchValue);
-
+var responseContainer = document.querySelector("#current-result");
+var cityNameEl = document.createElement("span");
 
 // Make function to get weather api
-var weatherRequest = function(city) {
-    var weatherApi =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=c83c5006fffeb4aa44a34ffd6a27f135";
+var weatherRequest = function (city) {
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c83c5006fffeb4aa44a34ffd6a27f135";
     // fetch the response
     fetch(weatherApi)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        // log the fetch response
-        console.log(response);
-        // grab the id of the element the weather data will be displayed
-        var responseContainer = document.querySelector("#current-result");
-        // create element for the weather response
-        var cityName = document.createElement("span");
-        // set the element's attribute to the json data path
-        cityName.setAttribute("src", response.name);
-        console.log(cityName);
-        // append the element to html
-        responseContainer.appendChild(cityName);
-        
-        // handle any errors
-        // .catch(function(error) {
-        //     console.log(error)
-        //     alert("Something went wrong!")
-        // });
-    });  
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            // log the fetch response
+            console.log(response);
+            // grab the id of the element the weather data will be displayed
+            // create element for the weather response
+            
+            // set the element's attribute to the json data path
+            // cityNameEl.setAttribute("src", response.name);
+            cityNameEl.innerHTML = "<h2>" + response.name + "</h2>";
+            console.log(response.name);
+            // append the element to html
+            responseContainer.appendChild(cityNameEl);
+
+            // handle any errors
+            // .catch(function(error) {
+            //     console.log(error)
+            //     alert("Something went wrong!")
+            // });
+        });
 };
 
-var searchEvent = function() {
+var searchEvent = function (event) {
+    event.preventDefault();
     // clicking search button submits value and calls weatherRequest function
-    console.log("test");
-    // event.target.querySelector("#search-button");
-    weatherRequest();
+    var searchValue = searchBar.value.trim();
+    if (searchValue) {
+        console.log(searchValue);
+        weatherRequest(searchValue);
+    } else {
+        //if search is empty, throw an alert. CHANGE TO A MODAL LATER
+        alert("Please enter a city to see its current weather.");
+    }
+
+    // clear search bar after clicking search button
+    document.querySelector("#search-bar").value = "";
+    // call function to remove previously searched weather
+    removePrevious();
 };
 
-searchButton.addEventListener("click", searchEvent);
+// remove previously searched weather info
+var removePrevious = function() {
+    cityNameEl.remove();
+    
+};
+
+searchHandler.addEventListener("submit", searchEvent);
 
 // weatherRequest();
